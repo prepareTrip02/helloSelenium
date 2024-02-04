4bcf1fc27a108ddce933c9708c46385026979f38
a62kumar <atul.5.kumar.ext@nokia.com>
2024-02-04 16:58:56 +0530

    test

--- src/main/java/com/trip/prepare/SeleniumController.java
+++ src/main/java/com/trip/prepare/SeleniumController.java
@@ -1,20 +1,24 @@
 package com.trip.prepare;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 @RestController
 public class SeleniumController {	
-	
+	private static final Logger logger = LogManager.getLogger(SeleniumController.class);	
 	
 	@GetMapping("test")
 	void test() {
-		WebDriver driver=null;
+		
 		System.out.println("Inside test");
 		// Setup ChromeDriver using WebDriverManager
 		try {
@@ -22,16 +26,19 @@ public class SeleniumController {
 		
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless");
-		driver = new ChromeDriver(options);
+		WebDriver driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.get("https://google.com");
 		System.out.println(driver.getTitle());
 		Thread.sleep(2000);	
 		}catch (Exception e) {
+			System.out.println("Inside Exception");
+			e.printStackTrace();
+			logger.info(e);
 			// TODO: handle exception
 		}finally {
-			
-			driver.quit();
+			System.out.println("Inside finally");
+			//driver.quit();
 		}
 	}
 
